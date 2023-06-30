fc9e1fc3b96264ccf06be2c6619077acb0993fc2
maotterson <otterson.2@gmail.com>
2023-06-30 00:00:44 -0700

    modify implementation for addservices reflection

--- src/LazyWizards.BlazorServer/Utils/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/LazyWizards.BlazorServer/Utils/DependencyInjection/ServiceCollectionExtensions.cs
@@ -9,24 +9,46 @@ public static class ServiceCollectionExtensions
     {
         var a = Assembly.GetCallingAssembly();
         var injectables = GetInjectableTypes(a);
-        injectables
-            .ToList()
-            .ForEach((injectable) => {
-                switch(injectable.attr.RegistrationType){
-                    case DependencyRegistrationTypes.Singleton:
-                        var type = injectable.type.GetType();
-                        services.AddSingleton<typeof(type)>();
-                        break;
-                    case DependencyRegistrationTypes.Transient:
-                    
-                        break;
-                    case DependencyRegistrationTypes.Scoped:
-                    
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
+        injectables.ToList()
+                .ForEach((injectable) => {
+            var implementation = injectable.type.GetType();
+            var contract = injectable.attr.ContractType;
+            switch(injectable.attr.RegistrationType)
+            {
+                case DependencyRegistrationTypes.Singleton:
+                    if(contract is null)
+                    {
+                        services.AddSingleton(implementation);
+                    }
+                    else
+                    {
+                        services.AddSingleton(contract, implementation);
+                    }
+                    break;
+                case DependencyRegistrationTypes.Transient:
+                    if(contract is null)
+                    {
+                        services.AddTransient(implementation);
+                    }
+                    else
+                    {
+                        services.AddTransient(contract, implementation);
+                    }
+                    break;
+                case DependencyRegistrationTypes.Scoped:
+                    if(contract is null)
+                    {
+                        services.AddScoped(implementation);
+                    }
+                    else
+                    {
+                        services.AddScoped(contract, implementation);
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException();
             }
+        });
     }
 
     private static IEnumerable<(Type type, Injectable attr)> GetInjectableTypes(Assembly assembly)
